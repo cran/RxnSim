1179972a7b2558cd6c5fa25202c12e8c343f9722
Varun Giri <varungiri@gmail.com>
2023-07-19 22:40:05 +0000

    version 1.0.4

--- R/RxnSimMask.R
+++ R/RxnSimMask.R
@@ -1,49 +1,69 @@
 .mask <- function (mol, map, mask) {
   count <- .jcall(mol, 'I', 'getAtomCount')
-  atoms <- c(0:(count-1))
+  atoms <- c(0:(count - 1))
   atoms <- atoms[!atoms %in% map$mapping[[1]]]
   
   if (!is.null(mask)) {
     .jcall(mol, 'V', 'add', mask)
-    newAtom <- .jcall(mol, 'Lorg/openscience/cdk/interfaces/IAtom;', 'getLastAtom')
+    newAtom <-
+      .jcall(mol,
+             'Lorg/openscience/cdk/interfaces/IAtom;',
+             'getLastAtom')
   }
   
   mapping <- sort(map$mapping[[1]], TRUE)
   newAtomContainer <- .jnew('org/openscience/cdk/AtomContainer')
   
   for (atomNo in mapping) {
-    atom <- .jcall(mol, 'Lorg/openscience/cdk/interfaces/IAtom;', 'getAtom', atomNo)
-    connectedAtoms <- as.list(.jcall(mol, 'Ljava/util/List;', 'getConnectedAtomsList', atom))
+    atom <-
+      .jcall(mol,
+             'Lorg/openscience/cdk/interfaces/IAtom;',
+             'getAtom',
+             atomNo)
+    connectedAtoms <-
+      as.list(.jcall(mol, 'Ljava/util/List;', 'getConnectedAtomsList', atom))
     for (nbdAtom in connectedAtoms) {
       nbdAtom <- .jcast(nbdAtom, 'org/openscience/cdk/interfaces/IAtom')
       nbdAtomNo <- .jcall(mol, 'I', 'getAtomNumber', nbdAtom)
-      bond <- .jcall(mol, 'Lorg/openscience/cdk/interfaces/IBond;', 'removeBond', atom, nbdAtom)
+      bond <-
+        .jcall(mol,
+               'Lorg/openscience/cdk/interfaces/IBond;',
+               'removeBond',
+               atom,
+               nbdAtom)
       if (!nbdAtomNo %in% map$mapping[[1]]) {
         if (!is.null(mask)) {
-          .jcall(bond, 'V', 'setAtoms', .jarray(c(nbdAtom, newAtom), contents.class = 'org/openscience/cdk/interfaces/IAtom'))
+          .jcall(
+            bond,
+            'V',
+            'setAtoms',
+            .jarray(c(nbdAtom, newAtom), contents.class = 'org/openscience/cdk/interfaces/IAtom')
+          )
           .jcall(mol, 'V', 'addBond', bond)
         }
       }
     }
     .jcall(newAtomContainer, 'V', 'addAtom', atom)
   }
-  nAC <- .jcast(newAtomContainer, 'org/openscience/cdk/interfaces/IAtomContainer')
+  nAC <-
+    .jcast(newAtomContainer,
+           'org/openscience/cdk/interfaces/IAtomContainer')
   .jcall(mol, 'V', 'remove', nAC)
 }
 
 .meta.mask <- function(substructure, mask, mol, recursive) {
   smi <- tryCatch({
-    while(1) {
+    while (1) {
       if (mask != '') {
-        maskX <- .smilesParser(mask, F, F)
+        maskX <- .smilesParser(mask, FALSE, FALSE)
       } else {
         maskX <- NULL
       }
       
       tryCatch({
-        map <- rcdk::matches(substructure, mol, return.matches = T)
+        map <- rcdk::matches(substructure, mol, return.matches = TRUE)
       }, error = function(err) {
-        stop('Unable to find matches.', call. = F)
+        stop('Unable to find matches.', call. = FALSE)
         #stop(err)
       })
       if (map[[1]]$match == TRUE) {
@@ -51,7 +71,7 @@
       } else {
         break
       }
-      if(recursive == FALSE) {
+      if (recursive == FALSE) {
         break
       }
     }
@@ -63,35 +83,44 @@
   return(smi)
 }
 
-ms.mask <- function (substructure, mask, molecule, format = 'smiles', standardize = T, explicitH = F, recursive = F) {
-  if(missing(substructure) || substructure == '') {
-    stop('Enter a structure to mask in form of a SMILES or SMARTS.', call. = F)
-  }
-  if(missing(mask)) {
-    stop('Mask not specified.', call. = F)
-  }
-  if(missing(molecule)) {
-    stop('Input molecule missing.', call. = F)
-  }
-  
-  format <- tolower(format)
-  
-  
-  smi <- tryCatch({    
-    if (format[[1]] == 'smiles') {
-      mol <- .smilesParser(molecule, standardize = standardize, explicitH = explicitH)
-    } else if (format[[1]] == 'mol') {
-      mol <- .molParser(molecule, standardize = standardize, explicitH = explicitH)
-    } else {
-      stop("Invalid input format.", call. = F)
+ms.mask <-
+  function (substructure,
+            mask,
+            molecule,
+            format = 'smiles',
+            standardize = TRUE,
+            explicitH = FALSE,
+            recursive = FALSE) {
+    if (missing(substructure) || substructure == '') {
+      stop('Enter a structure to mask in form of a SMILES or SMARTS.', call. = FALSE)
     }
-    mask <- sub('\\s+', '', mask)
-    .meta.mask(substructure, mask, mol, recursive)
-  }, error = function (err) {
+    if (missing(mask)) {
+      stop('Mask not specified.', call. = FALSE)
+    }
+    if (missing(molecule)) {
+      stop('Input molecule missing.', call. = FALSE)
+    }
+    
+    format <- tolower(format)
+    
+    
+    smi <- tryCatch({
+      if (format[[1]] == 'smiles') {
+        mol <-
+          .smilesParser(molecule, standardize = standardize, explicitH = explicitH)
+      } else if (format[[1]] == 'mol') {
+        mol <-
+          .molParser(molecule, standardize = standardize, explicitH = explicitH)
+      } else {
+        stop("Invalid input format.", call. = FALSE)
+      }
+      mask <- sub('\\s+', '', mask)
+      .meta.mask(substructure, mask, mol, recursive)
+    }, error = function (err) {
       stop (err)
-  })
-  return(smi)
-}
+    })
+    return(smi)
+  }
 
 .rct.mask <- function (substructure, mask, rct, recursive) {
   rct <- tryCatch({
@@ -104,7 +133,8 @@ ms.mask <- function (substructure, mask, molecule, format = 'smiles', standardiz
       .meta.mask(substructure, mask, mol, recursive)
     }
     
-    react <- paste(lapply(rct$Reactants, get.smiles), collapse = '.')
+    react <-
+      paste(lapply(rct$Reactants, get.smiles), collapse = '.')
     react <- sub('\\.+', '.', react)
     react <- sub('^\\.', '', react)
     react <- sub('\\.$', '', react)
@@ -114,7 +144,7 @@ ms.mask <- function (substructure, mask, molecule, format = 'smiles', standardiz
     prod <- sub('^\\.', '', prod)
     prod <- sub('\\.$', '', prod)
     
-    rct$RSMI <- paste(react, prod, sep=">>")
+    rct$RSMI <- paste(react, prod, sep = ">>")
     rct
   }, error = function (err) {
     stop (err)
@@ -122,32 +152,41 @@ ms.mask <- function (substructure, mask, molecule, format = 'smiles', standardiz
   return(rct)
 }
 
-rs.mask <- function (substructure, mask, reaction, format = 'rsmi', standardize = T, explicitH = F, recursive = F) {
-  if(missing(substructure) || substructure == '') {
-    stop('Enter a structure to mask in form of a SMILES or SMARTS.', call. = F)
-  }
-  if(missing(mask)) {
-    stop('Mask not specified.', call. = F)
-  }
-  if(missing(reaction)) {
-    stop('Input reaction missing.', call. = F)
-  }
-  
-  format <- tolower(format)
-  
-  rsmi <- tryCatch({
-    if (format[[1]] == 'rsmi') {
-      rct <- .rsmiParser(reaction, standardize = standardize, explicitH = explicitH)
-    } else if (format[[1]] == 'rxn') {
-      rct <- .mdlParser(reaction, standardize = standardize, explicitH = explicitH)
-    } else {
-      stop("Invalid input format.", call. = F)
+rs.mask <-
+  function (substructure,
+            mask,
+            reaction,
+            format = 'rsmi',
+            standardize = TRUE,
+            explicitH = FALSE,
+            recursive = FALSE) {
+    if (missing(substructure) || substructure == '') {
+      stop('Enter a structure to mask in form of a SMILES or SMARTS.', call. = FALSE)
+    }
+    if (missing(mask)) {
+      stop('Mask not specified.', call. = FALSE)
+    }
+    if (missing(reaction)) {
+      stop('Input reaction missing.', call. = FALSE)
     }
     
-    rct <- .rct.mask(substructure, mask, rct, recursive)
-    rct$RSMI
-  }, error = function (err) {
-    stop (err)
-  })
-  return(rsmi)
-}
+    format <- tolower(format)
+    
+    rsmi <- tryCatch({
+      if (format[[1]] == 'rsmi') {
+        rct <-
+          .rsmiParser(reaction, standardize = standardize, explicitH = explicitH)
+      } else if (format[[1]] == 'rxn') {
+        rct <-
+          .mdlParser(reaction, standardize = standardize, explicitH = explicitH)
+      } else {
+        stop("Invalid input format.", call. = FALSE)
+      }
+      
+      rct <- .rct.mask(substructure, mask, rct, recursive)
+      rct$RSMI
+    }, error = function (err) {
+      stop (err)
+    })
+    return(rsmi)
+  }
---
+++ README.md
@@ -0,0 +1,2 @@
+# RxnSim
+Methods to compute chemical similarity between two or more reactions and molecules. Depends upon 'rCDK' and 'fingerprint' for cheminformatics functionality.
